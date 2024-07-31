df2117cdfeb3bee7c962669e08d490b66e7e7d9d
Jasson-Chou <ss5014871@gmail.com>
2024-07-31 17:45:09 +0800

    first time upload

---
+++ InternalProcessCommunicationWithTCP/IPCLib/TCPIP/TCPClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPCLib.TCPIP
+{
+    public class TCPClient
+    {
+        private readonly bool clientInstance;
+        private TcpClient client;
+        private NetworkStream stream;
+
+        internal TCPClient(TcpClient client)
+        {
+            this.client = client;
+            clientInstance = false;
+        }
+
+        public TCPClient(int port)
+        {
+            this.client = new TcpClient();
+            clientInstance = true;
+        }
+
+        public async Task<bool> ConnectAsync(int port)
+        {
+            try
+            {
+                await client.ConnectAsync("127.0.0.1", port);
+                stream = client.GetStream();
+                return true;
+            }
+            catch(Exception e)
+            {
+                Trace.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+        public async Task<bool> DisconnectAsync()
+        {
+            try
+            {
+                // 發送斷開連接的消息
+                string disconnectMessage = "DISCONNECT";
+                byte[] data = Encoding.ASCII.GetBytes(disconnectMessage);
+                await stream.WriteAsync(data, 0, data.Length);
+                Console.WriteLine("已發送斷開連接請求");
+
+                // 關閉流和客戶端
+                stream.Close();
+                client.Close();
+                Console.WriteLine("已斷開與服務器的連接");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"斷開連接錯誤: {e.Message}");
+                return false;
+            }
+        }
+
+    }
+}
---
+++ InternalProcessCommunicationWithTCP/IPCLib/TCPIP/TCPServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPCLib.TCPIP
+{
+    public class TCPServer
+    {
+        private TcpListener server = null;
+
+        public event Action<TcpClient> OnClientConnect = null;
+
+        public TCPServer(int port)
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            this.Port = port;
+            server = new TcpListener(localAddr, Port);
+            Clients = new List<TcpClient>();
+        }
+
+        public int Port { get; }
+        public List<TcpClient> Clients { get; }
+
+        public async Task WaitClientConnection()
+        {
+            while (true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                Clients.Add(client);
+                OnClientConnect?.Invoke(client);
+            }
+        }
+
+
+    }
+}
